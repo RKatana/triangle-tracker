bd57c371d81e9fd77b95586b07161390a69e34f1
Raphael Katana <roduor41@gmail.com>
2018-09-14 16:34:01 +0300

    add if statement

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,6 @@ console.log(side2);
 var side3=document.querySelector().value{
   console.log(side3);
 }
+if (side1==side2&&side1==side3&&side2==side3) {
+  alert('Equilateral: All sides are equal!');
+}
