8f66db7d8427c0c525826603cff42414e1156e31
Raphael Katana <roduor41@gmail.com>
2018-09-14 22:09:54 +0300

    add css style to nav

--- js/scripts.js
+++ js/scripts.js
@@ -8,13 +8,13 @@ if(side1+side2>side3 && side2+side3>side1 && side1+side3>side2) {
     answer.textContent='Equilateral: All sides are equal!';
   }
   else if(side1===side2||side2===side3||side1===side3){
-    answer.textContent='Isosceles: Two sides are equal.'
+    answer.textContent='Isosceles: Two sides are equal!'
   }
   else{
-    answer.textContent="Scalene: No sides are equal";
+    answer.textContent="Scalene: No sides are equal!";
   }
 }
  else {
-  answer.textContent="Not a triangle";
+  answer.textContent="Not a triangle!";
 }
 }
