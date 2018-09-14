0ccc87b63f18e0b1afcfd485b1ce71e472a064cc
Raphael Katana <roduor41@gmail.com>
2018-09-14 17:12:25 +0300

    add value to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,9 @@
-var side1=document.querySelector().value{
-console.log(side1);
+function result(){
+var side1=parseInt(document.getElementById('side1').value);
+var side2=parseInt(document.getElementById('side2').value);
+var side3=parseInt(document.getElementById('side3').value);
+var answer=document.getElementById('answer');
+if (side1==side2&&side1==side3) {
+  answer.textContent='Equilateral: All sides are equal!';
 }
-var side2=document.querySelector().value{
-console.log(side2);
-}
-var side3=document.querySelector().value{
-  console.log(side3);
-}
-if (side1==side2&&side1==side3&&side2==side3) {
-  alert('Equilateral: All sides are equal!');
 }
